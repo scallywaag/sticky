ac19f3bc6cf83c1eeb864f12ebb43f4fb77c4bd2
patrik <38130262+scallywaag@users.noreply.github.com>
2025-05-07 20:03:51 +0300

    create cli flags

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
+
+type flags struct {
+	add  string
+	get  int
+	list bool
+	del  int
+}
 
 func main() {
-	fmt.Println("hello, world")
+	f := new(flags)
+	flag.StringVar(&f.add, "add", "", "add a note")
+	flag.IntVar(&f.get, "get", 0, "get a note by id")
+	flag.BoolVar(&f.list, "list", false, "list all notes")
+	flag.IntVar(&f.del, "del", 0, "delete a note by id")
+	flag.Parse()
+
+	switch {
+	case f.add != "":
+		fmt.Println("flag add")
+	case f.get != 0:
+		fmt.Println("flag get")
+	case f.list:
+		fmt.Println("flag list")
+	case f.del != 0:
+		fmt.Println("flag del")
+	default:
+		fmt.Println("default - flag list")
+	}
 }
