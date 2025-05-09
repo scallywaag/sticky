4bc68a9c20c7df8c9986e3f199cd7060f6dfd0d0
patrik <38130262+scallywaag@users.noreply.github.com>
2025-05-09 14:36:22 +0300

    refactor formatter

--- internal/formatter/formatter.go
+++ internal/formatter/formatter.go
@@ -1,29 +1,36 @@
 package formatter
 
 import (
+	"example/sticky/internal/colors"
 	"fmt"
 	"strconv"
 )
 
-func Print(content string, firstId int, lastId int) {
-	s := ""
-	ps := getPaddingSize(firstId, lastId)
-
-	for i := 0; i < ps; i++ {
-		s += " "
-	}
-
-	fmt.Printf("%s%d - %s\n", s, firstId, content)
+func Print(content string, curr int, last int) {
+	d := getPadDiff(strconv.Itoa(curr), strconv.Itoa(last))
+	s := fmt.Sprintf("%s%d - %s\n", colors.Blue, curr, content)
+	ps := leftPad(s, d)
+	fmt.Printf("%s", ps)
 }
 
-func getPaddingSize(firstId int, lastId int) int {
-	f := len(strconv.Itoa(firstId))
-	l := len(strconv.Itoa(lastId))
-
+func getPadDiff(a string, b string) int {
+	la := len(a)
+	lb := len(b)
 	diff := 0
-	if f < l {
-		diff = l - f
+
+	if la < lb {
+		diff = lb - la
 	}
 
 	return diff
+}
+
+func leftPad(s string, l int) string {
+	ps := ""
+
+	for range l {
+		ps += " "
+	}
+
+	return ps + s
 }
