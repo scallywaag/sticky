dca18716f7b7ab407d9a7b602da5c074230c9217
patrik <38130262+scallywaag@users.noreply.github.com>
2025-05-10 17:19:31 +0300

    refactor db package and use a friendly path

--- internal/database/db.go
+++ internal/database/db.go
@@ -2,15 +2,52 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/scallywaag/sticky/internal/formatter"
 )
 
 func InitDb() *sql.DB {
-	db, err := sql.Open("sqlite3", "sticky.sqlite")
+	dbPath := getDbPath()
+	dbExists := fileExists(dbPath)
+
+	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	createNotesTable(db)
+
+	if !dbExists {
+		fmt.Println(formatter.Blue + "Created 'sticky.db' database at: " + dbPath + formatter.Reset)
+	}
+
+	return db
+}
+
+func getDbPath() string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbPath := filepath.Join(homeDir, ".local", "share", "sticky", "sticky.db")
+	if err := os.MkdirAll(filepath.Dir(dbPath), os.ModePerm); err != nil {
+		log.Fatalf("Error creating directory: %v", err)
+	}
+
+	return dbPath
+}
+
+func fileExists(path string) bool {
+	info, err := os.Stat(path)
+	return err == nil && !info.IsDir()
+}
+
+func createNotesTable(db *sql.DB) {
 	stmt := `
 	CREATE TABLE IF NOT EXISTS notes (
 		id integer NOT NULL PRIMARY KEY,
@@ -20,10 +57,8 @@ func InitDb() *sql.DB {
 	);
 	`
 
-	_, err = db.Exec(stmt)
+	_, err := db.Exec(stmt)
 	if err != nil {
 		log.Printf("%q: %s\n", err, stmt)
 	}
-
-	return db
 }
