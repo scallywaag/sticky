3e6cb6960c90ae0397149ec389328c5716b0fdbf
patrik <38130262+scallywaag@users.noreply.github.com>
2025-08-23 00:06:24 +0300

    update flags mutation description

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -45,9 +45,9 @@ func Parse() *Flags {
 		fmt.Fprintf(os.Stderr, "  -d <id> int\n\tdelete note by <id>\n")
 		fmt.Fprintf(os.Stderr, "  -m <id> int\n\tmutate existing note <id>\n")
 
-		fmt.Fprintf(os.Stderr, "\n* mutations\n\tuseable with -a or -m flag\n")
-		fmt.Fprintf(os.Stderr, "  -p bool\n\tpin note - send to top of list (toggle)\n")
-		fmt.Fprintf(os.Stderr, "  -c bool\n\tcross note - send to bottom of list (toggle)\n")
+		fmt.Fprintf(os.Stderr, "\n* mutations (toggle)\n\tuseable with -a or -m flag\n")
+		fmt.Fprintf(os.Stderr, "  -p bool\n\tpin note - send to top of list\n")
+		fmt.Fprintf(os.Stderr, "  -c bool\n\tcross note - send to bottom of list\n")
 		fmt.Fprintf(os.Stderr, "  -r bool\n\tcolor note red\n")
 		fmt.Fprintf(os.Stderr, "  -g bool\n\tcolor note green\n")
 		fmt.Fprintf(os.Stderr, "  -b bool\n\tcolor note blue\n")
