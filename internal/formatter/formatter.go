480cfcbc0f1c00cd81360106048c8260a45c06ab
patrik <38130262+scallywaag@users.noreply.github.com>
2025-08-23 18:23:16 +0300

    add formatter option for strikethrough/cross

--- internal/formatter/formatter.go
+++ internal/formatter/formatter.go
@@ -7,6 +7,7 @@ import (
 )
 
 const (
+	Strike  = "\x1b[9m"
 	Red     = "\x1b[31m"
 	Green   = "\x1b[32m"
 	Yellow  = "\x1b[33m"
@@ -15,13 +16,15 @@ const (
 	Reset   = "\x1b[0m"
 )
 
-func Print(content string, currId int, lastId int, color string) {
+func Print(content string, currId int, lastId int, color string, cross bool) {
 	curr := strconv.Itoa(currId)
 	last := strconv.Itoa(lastId)
 
 	padDiff := getPadDiff(curr, last)
 	maxPad := getPadDiff("", last)
 	maxContent := 80 - len(last)
+	sep := " - "
+	sepNewline := strings.Repeat(" ", len(sep))
 
 	lines := []string{}
 
@@ -55,15 +58,17 @@ func Print(content string, currId int, lastId int, color string) {
 
 	// Print first line with ID + padding
 	if len(lines) > 0 {
-		formatted := fmt.Sprintf("%s%s - %s%s\n", color, curr, lines[0], Reset)
-		padded := leftPad(formatted, padDiff)
+		formatted := formatLine(lines[0], color, cross)
+		newContent := fmt.Sprintf("%s%s%s\n", curr, sep, formatted)
+		padded := leftPad(newContent, padDiff)
 		fmt.Print(padded)
 	}
 
 	// Print remaining lines, aligned under first
 	for i := 1; i < len(lines); i++ {
-		formatted := fmt.Sprintf("%s   %s%s\n", color, lines[i], Reset)
-		padded := leftPad(formatted, maxPad)
+		formatted := formatLine(lines[i], color, cross)
+		newContent := fmt.Sprintf("%s%s\n", sepNewline, formatted)
+		padded := leftPad(newContent, maxPad)
 		fmt.Print(padded)
 	}
 }
@@ -78,4 +83,12 @@ func getPadDiff(a, b string) int {
 
 func leftPad(str string, spaces int) string {
 	return strings.Repeat(" ", spaces) + str
+}
+
+func formatLine(content string, color string, cross bool) string {
+	formatted := fmt.Sprintf("%s%s%s", color, content, Reset)
+	if cross {
+		formatted = fmt.Sprintf("%s%s", Strike, formatted)
+	}
+	return formatted
 }
