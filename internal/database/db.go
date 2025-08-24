4658867539b7f3370cd17016a44e0a99c3d2b6bc
patrik <38130262+scallywaag@users.noreply.github.com>
2025-08-24 18:24:05 +0300

    refactor db layer, allow for env based db files

--- internal/database/db.go
+++ internal/database/db.go
@@ -6,6 +6,9 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
+	"github.com/highseas-software/sticky/internal/config"
+	"github.com/highseas-software/sticky/internal/env"
 )
 
 func InitDb() *sql.DB {
@@ -30,12 +33,38 @@ func InitDb() *sql.DB {
 }
 
 func getDbPath() string {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
+	appEnv := config.GetAppEnv()
+
+	var dbDir, filename string
+
+	switch appEnv {
+	case env.EnvProd:
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbDir = filepath.Join(homeDir, ".local", "share", "sticky")
+		filename = "sticky.db"
+
+	case env.EnvDev:
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbDir = cwd
+		filename = "sticky.db"
+
+	case env.EnvTest:
+		cwd, err := os.Getwd()
+		if err != nil {
+			log.Fatal(err)
+		}
+		dbDir = cwd
+		filename = "sticky.test.db"
 	}
 
-	dbPath := filepath.Join(homeDir, ".local", "share", "sticky", "sticky.db")
+	dbPath := filepath.Join(dbDir, filename)
+
 	if err := os.MkdirAll(filepath.Dir(dbPath), 0700); err != nil {
 		log.Fatalf("Error creating directory: %v", err)
 	}
